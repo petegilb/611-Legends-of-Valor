ada4295460bea9737db2e53a2fa0d86adff08709
Peter Gilbert <petegilb@bu.edu>
2021-04-12 21:26:40 -0400

    new map generation

--- CS611_LoV/source/Map.java
+++ CS611_LoV/source/Map.java
@@ -8,9 +8,12 @@ import lmh.interfaces.*;
 // the legend and the menu. Shows hero party position on the map.
 public class Map implements Drawable
 {
-  private final int BLOCKED_CELL_RATIO = 20,
-                    MARKET_CELL_RATIO = 30,
-                    COMMON_CELL_RATIO = 50;
+  private final int BLOCKED_CELL_RATIO = 25, //should be 16
+                    NEXUS_CELL_RATIO = 19, //should be 12
+                    KOULOU_CELL_RATIO = 14,
+                    CAVE_CELL_RATIO = 14,
+                    BUSH_CELL_RATIO = 14,
+                    COMMON_CELL_RATIO = 14;
 
   private int width, height;
   private Cell[][] map;
@@ -24,26 +27,47 @@ public class Map implements Drawable
   {
     int mapSize = width * height;
 
+    //assuming we have an 8x8 grid fixed for Legends of Valor...
+    //blocked cells are in between the "lanes"
     int numBlockedCells = Math.round (mapSize * BLOCKED_CELL_RATIO / (float) 100),
-        numMarketCells = Math.round (mapSize * MARKET_CELL_RATIO / (float) 100),
-        numCommonCells = Math.round (mapSize * COMMON_CELL_RATIO / (float) 100);
+            numBushCells = Math.round(mapSize * BUSH_CELL_RATIO / (float) 100),
+            numKoulouCells = Math.round(mapSize * KOULOU_CELL_RATIO / (float) 100),
+            numCaveCells = Math.round(mapSize * CAVE_CELL_RATIO / (float) 100),
+            numNexusCells = Math.round (mapSize * NEXUS_CELL_RATIO / (float) 100),
+            numCommonCells = Math.round (mapSize * COMMON_CELL_RATIO / (float) 100);
 
+    //create lists of each type of cell
     List<Cell> blockedCells = new ArrayList<Cell>(numBlockedCells);
-    List<Cell> marketCells = new ArrayList<Cell>(numMarketCells);
+    List<Cell> nexusCells = new ArrayList<Cell>(numNexusCells);
     List<Cell> commonCells = new ArrayList<Cell>(numCommonCells);
+    List<Cell> caveCells = new ArrayList<Cell>(numCaveCells);
+    List<Cell> bushCells = new ArrayList<Cell>(numBushCells);
+    List<Cell> koulouCells = new ArrayList<Cell>(numKoulouCells);
 
+    //populate those lists
     for (int i = 0; i < numBlockedCells; i++)
       blockedCells.add (new Cell(Cell.TYPE_BLOCKED));
 
-    for (int i = 0; i < numMarketCells; i++)
-      marketCells.add (new Cell(Cell.TYPE_MARKET));
+    for (int i = 0; i < numNexusCells; i++)
+      nexusCells.add (new Cell(Cell.TYPE_NEXUS));
 
     for (int i = 0; i < numCommonCells; i++)
       commonCells.add (new Cell(Cell.TYPE_COMMON));
 
+    for (int i = 0; i < numCaveCells; i++)
+      caveCells.add (new Cell(Cell.TYPE_CAVE));
+
+    for (int i = 0; i < numBushCells; i++)
+      bushCells.add (new Cell(Cell.TYPE_BUSH));
+
+    for (int i = 0; i < numKoulouCells; i++)
+      koulouCells.add (new Cell(Cell.TYPE_KOULOU));
+
+    //make a linkedlist that we will then shuffle for the non-preset tiles
     LinkedList<Cell> cells = new LinkedList<Cell>();
-    cells.addAll (blockedCells);
-    cells.addAll (marketCells);
+    cells.addAll (caveCells);
+    cells.addAll (bushCells);
+    cells.addAll (koulouCells);
     cells.addAll (commonCells);
     Collections.shuffle (cells);
 
@@ -51,10 +75,26 @@ public class Map implements Drawable
     this.width = width;
     this.height = height;
 
-    for (int i = 0; i < height; i++)
-    {
-      for (int j = 0; j < width; j++)
-        map[i][j] = cells.removeFirst();
+    //set the tiles in the board itself
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if(i == 0 || i == height-1){ //if it's the first or the last row
+          if(j == 2 || j==5){ //assuming the grid is 8x8 for now
+            //if it's a blocked column
+            map[i][j] = new Cell(Cell.TYPE_BLOCKED);
+          }
+          else{ //otherwise it's a nexus
+            map[i][j] = new Cell(Cell.TYPE_NEXUS);
+          }
+
+        }
+        else if(j == 2 || j==5){ //if it's a blocked column
+          map[i][j] = new Cell(Cell.TYPE_BLOCKED);
+        }
+        else{
+          map[i][j] = cells.removeFirst();
+        }
+      }
 
       map[i][width-1].setEndOfRow(true);
     }
@@ -69,6 +109,7 @@ public class Map implements Drawable
 
     Cell cell = map[row][col];
 
+    //if the current location is where the heroes are located
     if (cell.getDisplayValue() == Cell.TYPE_HEROES)
       return cell;
 
@@ -81,25 +122,55 @@ public class Map implements Drawable
   public void draw()
   {
     System.out.println();
-
-    for (int i = 0; i < height; i++)
-    {
-      for (int j = 0; j < width; j++)
-        System.out.print("+---");
-
-      System.out.println("+");
-
-      for (int j = 0; j < width; j++)
+    String spaces = "   ";
+    for (int i = 0; i < height; i++) {
+      if(i!=0){
+        System.out.println("");
+      }
+      //for each cell first row
+      for (int j = 0; j < width; j++){
         map[i][j].draw();
+      }
+
+      //for each cell second row
+      for (int j = 0; j < width; j++){
+        //if the cell is blocked
+        if(map[i][j].getType() == map[i][j].TYPE_BLOCKED){
+          System.out.print("| X X X |");
+        }
+        else{
+          System.out.print("|       |");
+        }
+
+        //if there is a hero or monster in the first slot
+
+
+        //in the second slot
+
+        //if it's the end of the row add a space
+        if(map[i][j].isEndOfRow() == false){
+          System.out.print("   ");
+        }
+        else{
+          System.out.print("\n");
+        }
+      }
+
+      //for each cell third row
+      for (int j = 0; j < width; j++){
+        map[i][j].draw();
+      }
     }
 
-    for (int i = 0; i < width; i++)
-      System.out.print("+---");
+//    for (int i = 0; i < width; i++){
+//      System.out.print("+---");
+//    }
 
-    System.out.println("+");
+    //System.out.println("+");
     displayLegendAndMenu();
   }
 
+  //to be updated...
   private void displayLegendAndMenu ()
   {
     System.out.print("\nSquares marked with " + Cell.TYPE_BLOCKED + " are not accessible. ");
