ba78b5680ebca9880bbe618af37b3d275161a4ba
Yuan Wei <yuanwei@bu.edu>
2021-04-14 00:48:51 -0400

    add shell script

--- 611-Legends-of-Valor/test.sh
+++ 611-Legends-of-Valor/test.sh
@@ -3,5 +3,6 @@ cd /Users/yuanwei/IdeaProjects/611-Legends-of-Valor-map/CS611_LoV
 find . -name "*.class" -type f -delete
 find . -name "*.java" > sources.txt
 # if first running : mkdir classes
+mkdir classes
 javac -d classes -cp classes @sources.txt
 java -cp classes lmh.Play
